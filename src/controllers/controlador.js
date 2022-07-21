ab08708bf13784cc301bfcef6ea4f78cc9e93471
cduquegiraldo <cduquegiraldo2@gmail.com>
2022-07-21 17:43:02 -0500

    se modifica el index

---
+++ src/controllers/controlador.js
@@ -0,0 +1,15 @@
+//creando variables - comentario de linea
+/*
+creando variable
+comentario de bloque
+*/
+let nombreUsuario="Angel Soto"
+
+const EDAD_USUARIO=26
+let estaturaUsuario=1.70
+let hinchaDelRojo=true
+
+console.log(nombreUsuario)
+
+EDAD_USUARIO=20
+console.log(EDAD_USUARIO)
